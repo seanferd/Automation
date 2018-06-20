8ff9b1147645249a71a778cc35062a25759a5ef8
Ford <fords@umh.edu>
2018-06-20 16:06:37 -0500

    Adding Import / Export scripts

---
+++ Import tasks.ps1
@@ -0,0 +1,4 @@
+﻿foreach ( $Task in get-childitem "X:\Users\Sean Ford\Scheduled Tasks\*.xml" ){
+#echo $Task.BaseName
+Register-ScheduledTask -Xml (get-content $Task | out-string) -TaskPath \Automation\ -TaskName $task.BaseName -User "umhs-users\umhc_hris"
+}
