6a490bdbfa28c1fce6aad017bbe74f279874ac37
Sean R Ford <sford_kc@hotmail.com>
2018-06-20 16:08:05 -0500

    Adding Import / Export scripts

---
+++ Export tasks.ps1
@@ -0,0 +1,11 @@
+﻿    #<############################################## Export Scheduled Task ##############################################
+    $ExportPath = "X:\Users\Sean Ford\Scheduled Tasks"           # Set export path, you need to have access to it.
+    $Tasks = Get-ScheduledTask #–TaskPath "\FolderName\" # Set task path (folder name) as it have in Task Scheduler or leave it as "\" if it placed in root.
+
+    # Create path if not exist
+    If((Test-Path $ExportPath) -ne $True){New-Item -ItemType directory -Path $ExportPath}
+
+    Foreach($Task in $Tasks) 
+    {
+        Export-ScheduledTask -TaskName $Task.TaskName -TaskPath $Task.TaskPath | Out-File (Join-Path $ExportPath "$($Task.TaskName).xml")
+    }
