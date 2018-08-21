54041d72963366e8cbc18a9b2d745f253aee084e
Sean R Ford <sford_kc@hotmail.com>
2018-08-21 15:46:41 -0500

    Add new batch file

---
+++ map-network-drives.bat
@@ -0,0 +1,19 @@
+net use A: /d /y
+net use H: /d /y
+net use M: /d /y
+net use O: /d /y
+net use P: /d /y
+net use T: /d /y
+net use U: /d /y
+net use X: /d /y
+net use Z: /d /y
+
+net use A: "\\umhc-install01\install" *
+net use H: "\\umh.edu\data\Personnel_Payroll\Salary\HRIS" *
+net use M: "\\umh.edu\data" *
+net use O: "\\umh.edu\data\Personnel_Payroll" *
+net use P: "\\umh.edu\global\Organization" *
+net use T: "\\umh.edu\data\Personnel_Payroll\Salary\Users\Sean Ford" *
+net use U: "\\umh.edu\global" *
+net use X: "\\umh.edu\data\Personnel_Payroll\Salary" *
+net use Z: "\\col.missouri.edu\files\pscommon\input\UM_PHOTOS\UMH_PHOTOS" *
